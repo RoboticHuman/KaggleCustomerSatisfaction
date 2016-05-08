5da2c76311d7447dd6148a6a5c83c8146b29e4dc
RoboticHuman <donomar@aucegypt.edu>
2016-05-08 18:50:49 +0200

    latest change with xgboost working and logistic regression

--- GBLR.py
+++ GBLR.py
@@ -1,4 +1,31 @@
 '''Gradient Boosted Logistic Regression '''
+def MCCR(predicted, expected, C1=-1, C2=1):
+    c1_hit, c1_miss, c2_hit, c2_miss = 0,0,0,0
+    TP,TN,FP,FN = 0,0,0,0
+    for (p, e) in zip(predicted, expected):
+        if p == e:
+            if e == C1:
+                 c1_hit += 1
+                 TP += 1
+            else:
+                 c2_hit += 1
+                 TN += 1
+        else:
+             if e == C1:
+                 c1_miss += 1
+                 FN += 1
+             else:
+                 c2_miss += 1
+                 FP += 1
+             CCR1 = float(c1_hit) / (c1_hit+c1_miss)
+             CCR2 = float(c2_hit) / (c2_hit+c2_miss)
+             TPR = float(TP)/(TP+FN)
+             FPR = float(FP)/(FP+TN)
+             MCCR = CCR1 if CCR1 < CCR2 else CCR2
+    return CCR1, CCR2, MCCR, TPR, FPR
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
 import numpy as np
 import csv
 import pickle
@@ -36,10 +63,12 @@ print "Finished loading data..."
 dtrain = xgb.DMatrix(trainds,trainlabels)
 dtest = xgb.DMatrix(testds,testlabels)
 dtestLabels = dtest.get_label();
-param = {'max_depth': 5,'gamma ':0.8,'booster':'gbtree','max_delta_step':10,'alpha':0.4,'lambda':0.4, 'eta': 1, 'silent': 1, 'objective': 'binary:logistic', 'eval_metric':'error'}
+param = {'max_depth': 5,'gamma ':0.8,'booster':'gbtree','max_delta_step':10,'alpha':0.4,'lambda':0.4, 'eta': 0.02, 'silent': 1, 'objective': 'binary:logistic', 'eval_metric':'auc'}
 watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 num_round = 5
+#clf = xgb.XGBClassifier(missing=np.nan, max_depth=5, n_estimators=350, learning_rate=0.03, nthread=4, subsample=0.95, colsample_bytree=0.85, seed=4242).fit(dtrain,trainlabels)
 bst = xgb.train(param, dtrain, num_round,watchlist)
+
 # this is prediction
 preds = bst.predict(dtest)
 labels = dtest.get_label()
@@ -50,26 +79,8 @@ plt.plot(x,y)
 plt.show()
 
 print (auc(x,y))
-
-print "MCCR"
-count1 = 0
-count2 = 0
-correct1 = 0
-correct2 = 0
-for index in range (len(preds)):
-    out = preds[index]
-    target = testlabels[index]
-    if target == 0:
-        count1 = count1 + 1
-        if out < 0.5:
-			correct1 = correct1 + 1
-    if target==1:
-        count2 = count2 + 1
-        if out >=0.5:
-			correct2 = correct2 + 1
-
-print correct1
-print correct2
-print count1
-print count2
-print min (float(correct1)/count1,float(correct2)/count2)
+predicted = preds > 0.5
+CCR1, CCR2, MCCR, TPR, FPR= MCCR(predicted,testlabels,0,1);
+print(CCR1)
+print CCR2
+print "MCCR: "+str(MCCR*100)
